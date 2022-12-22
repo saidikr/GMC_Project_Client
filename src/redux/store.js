32ddf3697457259ff5d6f405ac8cd7bcebef765a
said ikrouberkane <said.ikrouberkane@gmail.com>
2022-12-22 15:37:52 +0100

    redux-persist added

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,5 +17,12 @@ const persistConfig = {
 }
 const persistedReducer = persistReducer(persistConfig, cartReducer)
 export const store = configureStore({
-  reducer: cartReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+export const persistor = persistStore(store)
